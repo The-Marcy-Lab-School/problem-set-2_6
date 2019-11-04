5088c3036085173e3d07ffe30ef419cf7903f51f
Enmanuel de la Nuez <edelanuezlang@gmail.com>
2019-11-04 19:35:06 +0000

    complete 1-3

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,21 +1,48 @@
 // Question 1
-const log = () => {
-
+const log = (thingIn) => {
+  for (let prop in thingIn) {
+    console.log(prop +': '+thingIn[prop]);
+  }
 }
 
 // Question 2
-const totalProps = () => {
-
+const totalProps = (thingIn) => {
+  let counter = 0;
+  for (let prop in thingIn) {
+    counter ++;
+  }
+  return counter;
 }
 
 // Question 3
-const listBooks = () => {
-
+const listBooks = (arrayIn) => {
+  for (let i = 0; i < arrayIn.length; i ++) {
+    for (let prop in arrayIn[i]) {
+      console.log(arrayIn[i].title + ' was written by ' + arrayIn[i].author + '.');
+    }  
+  }
 }
 
 // Question 4
-const repeatedCharacters = () => {
-
+const repeatedCharacters = (wordIn) => {
+ let finalObj = {};
+ wordIn.toLowerCase().split('');
+ 
+ 
+ //for (let i = 0; i < wordIn.length; i ++) {
+   //lets check if a char is repeated and add it as a key to the obj
+   //if (wordIn[i]) {}
+   
+   
+   
+   //if we're the first char lets check it against all others
+   //for (let j = i; j < wordIn.length; j ++) {
+     //if (wordIn[j] === wordIn[i]) {
+       //finalObj[`$j`] += 1;
+     //}
+   //}
+ //}
+ 
 }
 
 // Don't write below this line
