7647c482dacb44e5c936ad00215e7e0c05339a8c
Ann Duong <30392423+thuyanduong@users.noreply.github.com>
2020-11-05 17:37:22 -0500

    Update exercises.test.js

--- problems/exercises.test.js
+++ problems/exercises.test.js
@@ -18,6 +18,23 @@ test('Question 1: `log` logs each property', () => {
   expect(global.console.log).toHaveBeenCalledWith('defense: 82');
 });
 
+test('Question 1: `log` logs different properties', () => {
+  global.console = { log: jest.fn() };
+
+  const attributes = {
+    speed: 91,
+    power: 91,
+    accuracy: 79,
+    defense: 82,
+  }
+
+  exercises.log(attributes);
+  expect(global.console.log).toHaveBeenCalledWith('speed: 91');
+  expect(global.console.log).toHaveBeenCalledWith('power: 91');
+  expect(global.console.log).toHaveBeenCalledWith('accuracy: 79');
+  expect(global.console.log).toHaveBeenCalledWith('defense: 82');
+});
+
 // Question 2
 test('Question 2: `totalProps` counts object properties', () => {
   const grades = {
@@ -81,4 +98,5 @@ test('Question 4: repeatedCharacters', () => {
   expect(exercises.repeatedCharacters('Pet')).toEqual({});
   expect(exercises.repeatedCharacters('Paper')).toEqual({ p: 2 });
   expect(exercises.repeatedCharacters('Baseless')).toEqual({ s: 3, e: 2 });
+  expect(exercises.repeatedCharacters('Baselesss')).toEqual({ s: 4, e: 2 });
 });
